8cafa7482d1422ba74015a97779d26e757ff733a
Anjali Tiwari <your_email@example.com>
2026-06-30 10:22:28 +0530

    add codes

--- lecture_2_if_else/absoluteValue.c
+++ lecture_2_if_else/absoluteValue.c
@@ -1,15 +1,15 @@
-#include<stdio.h>
-int main(){
-    int num ;
-    printf("enter the number : ");
-    scanf("%d",&num);
-    if(num<0){
-        num=num*(-1);
-        printf("number is negative  %d",num);
-    }
-    else{
-        printf("num is positive %d ",num);
+// #include<stdio.h>
+// int main(){
+//     int num ;
+//     printf("enter the number : ");
+//     scanf("%d",&num);
+//     if(num<0){
+//         num=num*(-1);
+//         printf("number is negative  %d",num);
+//     }
+//     else{
+//         printf("num is positive %d ",num);
 
-    }
-    return 0;
-}
+//     }
+//     return 0;
+// }
